13ea24138f333c99e78591330c225670905c1545
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2022-11-22 21:11:23 +1000

    Set up a “hover” effect

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 for (let i = 0; i < 256; i++) {
-    const grid = document.createElement('div');
-    container.appendChild(grid);
+  const grid = document.createElement('div');
+  container.appendChild(grid);
+  grid.addEventListener('mouseover', () => grid.classList.add('color-change'));
 }
