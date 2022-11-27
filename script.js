cf638f0f42fd5fd2b9f21303f82eb84134d965da
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2022-11-27 17:18:20 +1000

    Add grid changing button

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const container = document.querySelector('.container');
-for (let i = 0; i < 256; i++) {
-  const grid = document.createElement('div');
-  container.appendChild(grid);
-  grid.addEventListener('mouseover', () => grid.classList.add('color-change'));
+const button = document.querySelector('button');
+createGrid(16);
+button.onclick =
+() => changeGrid(prompt('Enter a number from 1 up to 100', "16"));
+
+function createGrid(squaresPerSide) {
+  for (let i = 0; i < squaresPerSide ** 2; i++) {
+    const square = document.createElement('div');
+    container.appendChild(square);
+    square.addEventListener('mouseover',
+    () => square.classList.add('color-change'));
+  }
+}
+
+function changeGrid(squaresPerSide) {
+  const squares = document.querySelectorAll('div div');
+  squares.forEach(square => container.removeChild(square));
+  container.setAttribute('style', 
+  'grid-template-columns: repeat(' + squaresPerSide + ', 1fr)');
+  createGrid(squaresPerSide); 
 }
