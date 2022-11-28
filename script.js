1fc20e1b9d13f282b869410ada8b8739febc983f
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2022-11-28 16:42:54 +1000

    Add conditions and restrictions for the prompt window

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.container');
 const button = document.querySelector('button');
 createGrid(16);
 button.onclick =
-() => changeGrid(prompt('Enter a number from 1 up to 100', "16"));
+() => changeGrid(prompt('Enter a number from 16 up to 100', "16"));
 
 function createGrid(squaresPerSide) {
   for (let i = 0; i < squaresPerSide ** 2; i++) {
@@ -14,9 +14,16 @@ function createGrid(squaresPerSide) {
 }
 
 function changeGrid(squaresPerSide) {
-  const squares = document.querySelectorAll('div div');
-  squares.forEach(square => container.removeChild(square));
-  container.setAttribute('style', 
-  'grid-template-columns: repeat(' + squaresPerSide + ', 1fr)');
-  createGrid(squaresPerSide); 
+  if (isNaN(squaresPerSide)) {
+    changeGrid(prompt('Enter a number from 16 up to 100'));
+  } else {
+    if (!squaresPerSide) return
+    if (squaresPerSide < 16) squaresPerSide = 16
+    if (squaresPerSide > 100) squaresPerSide = 100;
+    const squares = document.querySelectorAll('div div');
+    squares.forEach(square => container.removeChild(square));
+    container.setAttribute('style', 
+    'grid-template-columns: repeat(' + squaresPerSide + ', 1fr)');
+    createGrid(squaresPerSide); 
+  }
 }
