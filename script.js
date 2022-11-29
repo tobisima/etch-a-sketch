0ec369c7d08a5b27ef623643d74b0e994d8958c3
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2022-11-29 23:42:21 +1000

    Add multicolor drawing feature

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector('.container');
-const button = document.querySelector('button');
+const topButton = document.querySelector('.top-button');
+const MulticolorSetButton = document.querySelector('.multicolor-set-button');
+MulticolorSetButton.onclick = setMulticolorDrawing;
+topButton.onclick =
+() => changeGrid(prompt('Enter a number from 10 up to 100', "16"));
+
 createGrid(16);
-button.onclick =
-() => changeGrid(prompt('Enter a number from 16 up to 100', "16"));
 
 function createGrid(squaresPerSide) {
   for (let i = 0; i < squaresPerSide ** 2; i++) {
@@ -15,15 +18,25 @@ function createGrid(squaresPerSide) {
 
 function changeGrid(squaresPerSide) {
   if (isNaN(squaresPerSide)) {
-    changeGrid(prompt('Enter a number from 16 up to 100'));
+    changeGrid(prompt('Enter a number from 10 up to 100'));
   } else {
     if (!squaresPerSide) return
-    if (squaresPerSide < 16) squaresPerSide = 16
+    if (squaresPerSide < 10) squaresPerSide = 10
     if (squaresPerSide > 100) squaresPerSide = 100;
-    const squares = document.querySelectorAll('div div');
+    const squares = document.querySelectorAll('.container div');
     squares.forEach(square => container.removeChild(square));
-    container.setAttribute('style', 
-    'grid-template-columns: repeat(' + squaresPerSide + ', 1fr)');
-    createGrid(squaresPerSide); 
+    container.style = `grid-template-columns: repeat(${squaresPerSide}, 1fr)`;
+    createGrid(squaresPerSide);
   }
+}
+
+function set0_255() {
+  return Math.floor(Math.random() * 256);
+}
+
+function setMulticolorDrawing() {
+  const squares = document.querySelectorAll('.container div')
+  squares.forEach(square =>
+  square.addEventListener('mouseover', () =>
+  square.style.background = `rgb(${set0_255()}, ${set0_255()}, ${set0_255()})`));
 }
