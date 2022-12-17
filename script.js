4febbfd9afdf97cadd2f1c0df518d1b5894e4fd5
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2022-12-18 00:02:06 +1000

    Add shade to black feature

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.querySelector('.container');
 const topButton = document.querySelector('.top-button');
-const MulticolorSetButton = document.querySelector('.multicolor-set-button');
-MulticolorSetButton.onclick = setMulticolorDrawing;
-topButton.onclick =
-() => changeGrid(prompt('Enter a number from 10 up to 100', "16"));
-
+const multicolorButton = document.querySelector('.multicolor-button');
+const shadeToBlackButton = document.querySelector('.shade-to-black-button');
 createGrid(16);
+
+topButton.onclick = () =>
+changeGrid(prompt('Enter a number from 10 up to 100', "16"));
+shadeToBlackButton.onclick = setShadingToBlack;
+multicolorButton.onclick = setMulticolorDrawing;
 
 function createGrid(squaresPerSide) {
   for (let i = 0; i < squaresPerSide ** 2; i++) {
@@ -15,13 +17,13 @@ function createGrid(squaresPerSide) {
     () => square.classList.add('color-change'));
   }
 }
-
+  
 function changeGrid(squaresPerSide) {
   if (isNaN(squaresPerSide)) {
     changeGrid(prompt('Enter a number from 10 up to 100'));
   } else {
     if (!squaresPerSide) return
-    if (squaresPerSide < 10) squaresPerSide = 10
+    if (squaresPerSide < 10) squaresPerSide = 10;
     if (squaresPerSide > 100) squaresPerSide = 100;
     const squares = document.querySelectorAll('.container div');
     squares.forEach(square => container.removeChild(square));
@@ -30,13 +32,38 @@ function changeGrid(squaresPerSide) {
   }
 }
 
-function set0_255() {
-  return Math.floor(Math.random() * 256);
+function setMulticolorDrawing() {
+  const squares = document.querySelectorAll('.container div');
+    squares.forEach(square => {
+    square.removeEventListener('mouseover', addShadingValue);
+    square.addEventListener('mouseover', setRandomColor); 
+    });
 }
 
-function setMulticolorDrawing() {
-  const squares = document.querySelectorAll('.container div')
-  squares.forEach(square =>
-  square.addEventListener('mouseover', () =>
-  square.style.background = `rgb(${set0_255()}, ${set0_255()}, ${set0_255()})`));
+function setRandomColor(e) {
+  const random0_255 = () => Math.floor(Math.random() * 256);
+  e.target.style.background = `rgb(${random0_255()}, ${random0_255()}, ${random0_255()})`;
+} 
+
+function setShadingToBlack() {
+  const squares = document.querySelectorAll('.container div');
+    squares.forEach(square => { 
+    square.removeEventListener('mouseover', setRandomColor);
+    square.addEventListener('mouseover', addShadingValue);
+    });
 }
+
+function addShadingValue(e) {
+  const shadingValue = e.target.style.background.slice(-4, -1);
+  if (e.target.style.background === 'rgb(0, 0, 0)') return;
+  if (e.target.style.background.charAt(3) === '(') {
+    e.target.style.background = 'rgba(0, 0, 0, 0.1)';
+  } else {   
+    e.target.style.background = `rgba(0, 0, 0, ${+shadingValue + 0.1})`;
+  }
+}
+
+  
+
+  
+  
